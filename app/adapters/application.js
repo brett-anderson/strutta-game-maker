cc1be87f165e7754b3455216fe4f738b0f5a98ad
Brett Anderson <brettanderson16@gmail.com>
2015-10-01 11:15:26 -0700

    Update application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,6 +4,6 @@ export default DS.RESTAdapter.extend({
   namespace: 'v2',
   host: "http://api-beta.strutta.com",
   headers: {
-    Authorization: 'Token token=57cabf0660f67c1e189909d18419ffe4'
+    Authorization: 'Token token=@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
   },
 });
